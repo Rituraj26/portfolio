1971598f882f8d358ec9c0b7100e3d00c77a9abc
Rituraj <riturajguwahati26@gmail.com>
2019-05-30 11:52:17 +0530

    added title animations

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,3 +1,28 @@
-const navbar = document.querySelector('.navbar');
+const navbar = document.querySelector('.header__content');
+const title = document.querySelectorAll('.title');
+const underline = document.querySelectorAll('.underline');
+const skillsList = document.querySelector('.skills__list--li');
 
-ScrollReveal().reveal('navbar', {duration: 2000, origin: 'buttom'})
+window.sr = ScrollReveal();
+
+sr.reveal(navbar, {
+	duration: 2000, 
+	origin: 'buttom'
+});
+
+for(let i = 0; i < title.length; i++) {
+	sr.reveal(title[i], {
+		duration: 1000,
+		origin: 'left',
+		distance: '50rem'
+	});
+	sr.reveal(underline[i], {
+		delay: 500,
+		duration: 1000,
+		origin: 'left',
+		distance: '50rem'
+	});
+}
+// sr.reveal(skillsList, {
+// 	interval: 200
+// });
--- public/js/index.js
+++ public/js/index.js
@@ -1,80 +1,89 @@
 // Get DOM Elements
-const modal = document.querySelectorAll('.project__overlay');
+// const modal = document.querySelectorAll('.project__overlay');
 const modalBtn = document.querySelectorAll('.btn');
 const closeBtn = document.querySelectorAll('.project__overlay--close');
-// const leftArrow = document.querySelector('#left');
-// const rightArrow = document.querySelector('#right');
+const corousal = document.querySelectorAll('.project__overlay');
+const leftArrow = document.querySelector('#left');
+const rightArrow = document.querySelector('#right');
 
-// let current = 0;
+let current = 0;
 
 // clear all images
 
-// reset = () => {
-// 	for(let i = 0; i < imgPro1.length; i++) {
-// 		imgPro1[i].style.display = 'none';
-// 	}
-// }
+reset = () => {
+	for(let i = 0; i < corousal.length; i++) {
+		corousal[i].style.display = 'none';
+	}
+}
 
-// // initialize slider
+// initialize slider
 
-// startSlide = () => {
-// 	reset();
-// 	imgPro1[0].style.display = 'block';
-// }
+startSlide = () => {
+	reset();
+	corousal[0].style.display = 'block';
+}
 
-// slideLeft = () => {
-// 	reset();
-// 	imgPro1[current - 1].style.display = 'block';
-// 	current--;
-// }
+slideLeft = () => {
+	reset();
+	console.log(current);
+	corousal[current - 1].style.display = 'block';
+	current--;
+}
 
-// slideRight = () => {
-// 	reset();
-// 	imgPro1[current + 1].style.display = 'block';
-// 	current++;
-// }
+slideRight = () => {
+	reset();
+	console.log(current);
+	corousal[current + 1].style.display = 'block';
+	current++;
+	console.log(current, corousal);
+}
 
-// changeImageLeft = () => {
-// 	console.log(current);
-// 	if(current === 0) {
-// 		current = imgPro1.length;
-// 	}
-// 	slideLeft();
-// }
+changeImageLeft = () => {
+	console.log(current);
+	if(current === 0) {
+		current = corousal.length;
+	}
+	slideLeft();
+}
 
-// changeImageRight = () => {
-// 	if(current ===imgPro1.length - 1) {
-// 		current = -1;
-// 	}
-// 	slideRight();
-// }
+changeImageRight = () => {
+	console.log(current);
+	if(current === corousal.length - 1) {
+		current = -1;
+	}
+	slideRight();
+}
 
 // Events
 
-// leftArrow.addEventListener('click', (e) => changeImageLeft(e));
-// rightArrow.addEventListener('click', (e) => changeImageRight(e));
+
 for(let i = 0; i < modalBtn.length; i++) {
+	leftArrow.addEventListener('click', (e) => changeImageLeft(e));
+	rightArrow.addEventListener('click', (e) => changeImageRight(e));
 	// console.log(images[i]);
-	modalBtn[i].addEventListener('click', () => openModal(modal[i]));
-	closeBtn[i].addEventListener('click', () => closeModal(modal[i]));
-	window.addEventListener('click', (e) => outsideClick(e, modal[i]));
+	modalBtn[i].addEventListener('click', startSlide);
+	closeBtn[i].addEventListener('click', () => closeModal(corousal[i]));
+	window.addEventListener('click', (e) => outsideClick(e, corousal[i]));
 }
 
 // Open
-openModal = (m) => {
-	// reset();
-	// startSlide();
-  	m.style.display = 'block';
-}
+// openModal = (m) => {
+// 	console.log(current, m);
+// 	// reset();
+// 	startSlide();
+//   	m.style.display = 'block';
+// }
 
 // Close
 closeModal = (m) => {
 	m.style.display = 'none';
+	// reset();
 }
 
 // Close If Outside Click
 outsideClick = (e, m) => {
 	if (e.target == m) {
 		m.style.display = 'none';
+		// reset();
 	}
 }
