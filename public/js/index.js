38439ddeaccf3e8795f17481e1544b32ceb9deb6
Rituraj <riturajguwahati26@gmail.com>
2019-05-30 13:52:27 +0530

    first commit in new branch

--- public/js/index.js
+++ public/js/index.js
@@ -25,17 +25,14 @@ startSlide = () => {
 
 slideLeft = () => {
 	reset();
-	console.log(current);
 	corousal[current - 1].style.display = 'block';
 	current--;
 }
 
 slideRight = () => {
 	reset();
-	console.log(current);
 	corousal[current + 1].style.display = 'block';
 	current++;
-	console.log(current, corousal);
 }
 
 changeImageLeft = () => {
@@ -48,7 +45,7 @@ changeImageLeft = () => {
 
 changeImageRight = () => {
 	console.log(current);
-	if(current === corousal.length - 1) {
+	if(current ===corousal.length - 1) {
 		current = -1;
 	}
 	slideRight();
@@ -56,10 +53,9 @@ changeImageRight = () => {
 
 // Events
 
-
+leftArrow.addEventListener('click', (e) => changeImageLeft(e));
+rightArrow.addEventListener('click', (e) => changeImageRight(e));
 for(let i = 0; i < modalBtn.length; i++) {
-	leftArrow.addEventListener('click', (e) => changeImageLeft(e));
-	rightArrow.addEventListener('click', (e) => changeImageRight(e));
 	// console.log(images[i]);
 	modalBtn[i].addEventListener('click', startSlide);
 	closeBtn[i].addEventListener('click', () => closeModal(corousal[i]));
