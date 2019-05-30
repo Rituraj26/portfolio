16494d19a97331687d6d318efe4245244a70dd8a
Rituraj <riturajguwahati26@gmail.com>
2019-05-30 14:02:39 +0530

    fixed project section issue

--- public/js/index.js
+++ public/js/index.js
@@ -1,74 +1,74 @@
 // Get DOM Elements
-// const modal = document.querySelectorAll('.project__overlay');
+const modal = document.querySelectorAll('.project__overlay');
 const modalBtn = document.querySelectorAll('.btn');
 const closeBtn = document.querySelectorAll('.project__overlay--close');
-const corousal = document.querySelectorAll('.project__overlay');
-const leftArrow = document.querySelector('#left');
-const rightArrow = document.querySelector('#right');
+// const modal = document.querySelectorAll('.project__overlay');
+// const leftArrow = document.querySelector('#left');
+// const rightArrow = document.querySelector('#right');
 
-let current = 0;
+// let current = 0;
 
-// clear all images
+// // clear all images
 
-reset = () => {
-	for(let i = 0; i < corousal.length; i++) {
-		corousal[i].style.display = 'none';
-	}
-}
+// reset = () => {
+// 	for(let i = 0; i < modal.length; i++) {
+// 		modal[i].style.display = 'none';
+// 	}
+// }
 
-// initialize slider
+// // initialize slider
 
-startSlide = () => {
-	reset();
-	corousal[0].style.display = 'block';
-}
+// startSlide = () => {
+// 	reset();
+// 	modal[0].style.display = 'block';
+// }
 
-slideLeft = () => {
-	reset();
-	corousal[current - 1].style.display = 'block';
-	current--;
-}
+// slideLeft = () => {
+// 	reset();
+// 	modal[current - 1].style.display = 'block';
+// 	current--;
+// }
 
-slideRight = () => {
-	reset();
-	corousal[current + 1].style.display = 'block';
-	current++;
-}
+// slideRight = () => {
+// 	reset();
+// 	modal[current + 1].style.display = 'block';
+// 	current++;
+// }
 
-changeImageLeft = () => {
-	console.log(current);
-	if(current === 0) {
-		current = corousal.length;
-	}
-	slideLeft();
-}
+// changeImageLeft = () => {
+// 	console.log(current);
+// 	if(current === 0) {
+// 		current = modal.length;
+// 	}
+// 	slideLeft();
+// }
 
-changeImageRight = () => {
-	console.log(current);
-	if(current ===corousal.length - 1) {
-		current = -1;
-	}
-	slideRight();
-}
+// changeImageRight = () => {
+// 	console.log(current);
+// 	if(current ===modal.length - 1) {
+// 		current = -1;
+// 	}
+// 	slideRight();
+// }
 
-// Events
+// // Events
 
-leftArrow.addEventListener('click', (e) => changeImageLeft(e));
-rightArrow.addEventListener('click', (e) => changeImageRight(e));
+// leftArrow.addEventListener('click', (e) => changeImageLeft(e));
+// rightArrow.addEventListener('click', (e) => changeImageRight(e));
 for(let i = 0; i < modalBtn.length; i++) {
 	// console.log(images[i]);
-	modalBtn[i].addEventListener('click', startSlide);
-	closeBtn[i].addEventListener('click', () => closeModal(corousal[i]));
-	window.addEventListener('click', (e) => outsideClick(e, corousal[i]));
+	modalBtn[i].addEventListener('click', () => openModal(modal[i]));
+	closeBtn[i].addEventListener('click', () => closeModal(modal[i]));
+	window.addEventListener('click', (e) => outsideClick(e, modal[i]));
 }
 
 // Open
-// openModal = (m) => {
-// 	console.log(current, m);
-// 	// reset();
-// 	startSlide();
-//   	m.style.display = 'block';
-// }
+openModal = (m) => {
+	// console.log(current, m);
+	// reset();
+	// startSlide();
+  	m.style.display = 'block';
+}
 
 // Close
 closeModal = (m) => {
