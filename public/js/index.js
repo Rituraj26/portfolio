3f6a602bc4ab9a041c7a8e0fc40a935b013623c5
Rituraj <riturajguwahati26@gmail.com>
2020-07-29 21:34:43 +0530

    fixed project section responsivness

--- public/js/index.js
+++ public/js/index.js
@@ -36,3 +36,77 @@ outsideClick = (e, m) => {
         // reset();
     }
 };
+
+// Project Toggling
+
+let projectGrp1 = document.querySelectorAll('.projectGrp-1');
+let projectGrp2 = document.querySelectorAll('.projectGrp-2');
+let projectGrp3 = document.querySelectorAll('.projectGrp-3');
+let projectsBtn = document.querySelectorAll('.projects__btn');
+
+projectGrp1 = Array.from(projectGrp1);
+projectGrp2 = Array.from(projectGrp2);
+projectGrp3 = Array.from(projectGrp3);
+projectsBtn = Array.from(projectsBtn);
+
+const hideProjects = () => {
+    if (window.innerWidth > 1200) {
+        projectGrp2.forEach((project) => {
+            project.style.display = 'none';
+        });
+        projectGrp3.forEach((project) => {
+            project.style.display = 'none';
+        });
+    }
+};
+
+const toggleProjects = (id) => {
+    if (id === 1) {
+        projectGrp1.forEach((project) => {
+            project.style.display = 'grid';
+        });
+        projectGrp2.forEach((project) => {
+            project.style.display = 'none';
+        });
+        projectGrp3.forEach((project) => {
+            project.style.display = 'none';
+        });
+    } else if (id === 2) {
+        projectGrp1.forEach((project) => {
+            project.style.display = 'none';
+        });
+        projectGrp2.forEach((project) => {
+            project.style.display = 'grid';
+        });
+        projectGrp3.forEach((project) => {
+            project.style.display = 'none';
+        });
+    } else if (id === 3) {
+        projectGrp1.forEach((project) => {
+            project.style.display = 'none';
+        });
+        projectGrp2.forEach((project) => {
+            project.style.display = 'none';
+        });
+        projectGrp3.forEach((project) => {
+            project.style.display = 'grid';
+        });
+    }
+};
+
+// Project Responsivenes
+
+if (window.innerWidth <= 1200) {
+    projectsBtn.forEach((btn) => {
+        btn.style.display = 'none';
+    });
+    projectGrp1.forEach((project) => {
+        project.style.display = 'grid';
+    });
+    projectGrp2.forEach((project) => {
+        project.style.display = 'grid';
+    });
+    projectGrp3.forEach((project) => {
+        project.style.display = 'grid';
+    });
+}
